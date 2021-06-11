e1fdd5f2176a59851252344487796be5651718d8
exis <gotexis@gmail.com>
2021-06-11 13:04:23 +1000

    finish

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,22 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+
+  try {
+    const results = await Promise.all(urls.map(
+      url => httpGet(url)
+    ))
+
+    return results.map(
+      ({status, body}) => {
+        const {message} = JSON.parse(body)
+        return ({[status === 200 ? 'Arnie Quote' : 'FAILURE']: message});
+      }
+    );
+  } catch (e) {
+    console.error(e);
+  }
+
 };
 
 module.exports = {
